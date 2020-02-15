e959fac8ef003b6b396e85cbd10bfbf358d48bdc
Piotr Dąbrowski <piotrek816@gmail.com>
2020-02-15 12:25:56 +0100

    Add project files.

---
+++ GraffitiToMesOrdersSync/DoWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraffitiToMesOrdersSync
+{
+    public class DoWork
+    {
+        public static void StartSynchronization()
+        {
+            var grafOpenOrders = 
+        }
+    }
+}
---
+++ GraffitiToMesOrdersSync/GraffitiOrders/OpenOrders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraffitiToMesOrdersSync.GraffitiOrders
+{
+    public class OpenOrders
+    {
+        public List<string> IdList { get; set; } = new List<string>();
+        
+        public static void GetOpenOrdersIdList()
+        {
+
+        }
+    }
+}
